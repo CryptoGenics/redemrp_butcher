5c14d351b05092d15e1f7284e65684cc04243f32
CryptoGenics <45253987+CryptoGenics@users.noreply.github.com>
2021-02-28 22:46:50 -0600

    Item Loot / Configuration options

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -1,13 +1,10 @@
 fx_version "adamant"
+games {"rdr3"}
 rdr3_warning 'I acknowledge that this is a prerelease build of RedM, and I am aware my resources *will* become incompatible once RedM ships.'
-game "rdr3"
 
-client_scripts {
-	'config.lua',
-	'client.lua'
-}
+client_script {'client.lua'}
 
 server_scripts {
-	'server.lua',
-	'config.lua'
+    'server.lua',
+    'config.lua'
 }
--- server.lua
+++ server.lua
@@ -1,31 +1,73 @@
 local data = {}
-
 TriggerEvent("redemrp_inventory:getData",function(call)
 	data = call
 end)
 
-RegisterServerEvent("cryptos_butcher:giveitem")
-AddEventHandler("cryptos_butcher:giveitem", function(item, amount)
-	local _source = source
-	if Config.Redemrp_Inventory2 == false then
-		data.addItem(_source, item, amount)
-	elseif Config.Redemrp_Inventory2 == true then
-		local ItemInfo = data.getItemData(item)
-		local ItemData = data.getItem(_source, item)
-		if ItemData.ItemAmount >= ItemInfo.limit then
-			TriggerClientEvent("redemrp_notification:start", _source, "You already have to many "..item, 2, "error")
-		else
-			ItemData.AddItem(amount)
-		end
-	end
-end)
+RegisterServerEvent('cryptos_loot')
+AddEventHandler("cryptos_loot", function()
+  local _source = source
+  TriggerEvent('redemrp:getPlayerFromId', _source, function(user)
+    local get_item, lootpay, item, amount = false
 
-RegisterServerEvent("cryptos_butcher:reward")
-AddEventHandler("cryptos_butcher:reward", function(amount, xp)
-	local _source = source
-	local _amount = tonumber(string.format("%.2f", amount))
-	TriggerEvent('redemrp:getPlayerFromId', _source, function(user)
-		user.addMoney(_amount)
-		user.addXP(xp)
-	end)
+    -- Money
+    if Config.GiveMoney then
+      local loot = math.random(1, Config.MaxPayRoll)
+      local pay = loot / Config.PayDevider
+	  lootpay = tonumber(string.format("%.2f", pay))
+    end
+
+    -- Items
+    if Config.GiveItems then
+      local item_chance = math.random(98, 100)
+      if item_chance > 97 then
+        local max_items = tonumber(#Config.ItemPayout['2percent'])
+        local item_roll = math.random(1, max_items)
+		item, amount = Config.ItemPayout['2percent'][item_roll].item, Config.ItemPayout['2percent'][item_roll].amount
+		get_item = true
+      elseif item_chance > 90 then
+        local max_items = tonumber(#Config.ItemPayout['2percent'])
+        local item_roll = math.random(1, max_items)
+		item, amount = Config.ItemPayout['6percent'][item_roll].item, Config.ItemPayout['6percent'][item_roll].amount
+		get_item = true
+      elseif item_chance > 80 then
+        local max_items = tonumber(#Config.ItemPayout['2percent'])
+        local item_roll = math.random(1, max_items)
+		item, amount = Config.ItemPayout['10percent'][item_roll].item, Config.ItemPayout['10percent'][item_roll].amount
+		get_item = true
+      end
+    end
+
+    -- Give Loot
+    if get_item then
+      if Config.Redemrp_Inventory2 == false then
+        data.addItem(_source, item, tonumber(amount))
+        if Config.GiveMoney then
+          user.addMoney(lootpay)
+          TriggerClientEvent("redemrp_notification:start", _source, "You stole $"..lootpay.." and "..amount..' '..item, 3, "success")
+        else
+          TriggerClientEvent("redemrp_notification:start", _source, "You stole "..amount..' '..item, 3, "success")
+        end
+      elseif Config.Redemrp_Inventory2 == true then
+        local ItemInfo = data.getItemData(item)
+        local ItemData = data.getItem(_source, item)
+        if ItemData.ItemAmount >= ItemInfo.limit then
+          TriggerClientEvent("redemrp_notification:start", _source, "You already have to many "..item, 2, "error")
+        else
+          ItemData.AddItem(tonumber(amount))
+          if Config.GiveMoney then
+            user.addMoney(lootpay)
+            TriggerClientEvent("redemrp_notification:start", _source, "You stole $"..lootpay.." and "..amount..' '..item, 3, "success")
+          else
+            TriggerClientEvent("redemrp_notification:start", _source, "You stole "..amount..' '..item, 3, "success")
+          end
+        end  
+      end
+    else
+      if Config.GiveMoney then
+        user.addMoney(lootpay)
+        TriggerClientEvent("redemrp_notification:start", _source, "You stole $"..lootpay, 3, "success")
+      end  
+    end
+
+  end)
 end)
